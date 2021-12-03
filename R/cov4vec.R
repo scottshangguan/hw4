e6fbb6db3ae6ffcbc7c573a9911f674c1de37218
Chenhao Shangguan <cshanggu@umich.com>
2021-12-03 14:25:28 -0500

    1stfunction

---
+++ R/cov4vec.R
@@ -0,0 +1,26 @@
+pearson_cor<-function(x,y,method,stat_test){ #21
+  n=length(x)
+  mean_x=mean(x)
+  mean_y=mean(y)
+  num=sum((x-mean_x)*(y-mean_y))
+  denom=sqrt(sum((x-mean_x)*(x-mean_x))*sum((y-mean_y)*(y-mean_y)))
+  rho=num/denom
+  if(stat_test==FALSE){
+    RVAL<-list(correlation=rho,
+               method="pearson"
+    )
+    return(RVAL)
+  }
+  else{
+    df=n-2
+    t_stat = sqrt(df) * rho / sqrt(1 - rho^2)
+    p_value = 2*pt(-abs(t_stat),df)
+    RVAL <- list(correlation = rho,
+                 statistic = round(t_stat,4),
+                 df = df,
+                 p.value = round(as.numeric(p_value),2),
+                 method ="pearson"
+    )
+    return(RVAL)
+  }
+}
